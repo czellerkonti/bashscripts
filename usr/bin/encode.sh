1a1b0ff8b81250ab1bb34d8d27be582d733ac0c7
Konstantin Czeller <konstantin.czeller@lhsystems.com>
2017-04-09 23:29:41 +0200

    asd

--- usr/bin/encode.sh
+++ usr/bin/encode.sh
@@ -6,7 +6,8 @@ TEMPFILE=/mnt/data/temp.mp4
 TASK_LIST=/mnt/data/list.txt
 SEARCHDIR="$1"
 EXTENSIONS=".*\.\(avi\|mpg\|mp4\|mov\)$"
-FFMPEG=avconv
+FFMPEG=ffmpeg
+EXTRAOPTS="-v info -y"
 
 SUBDIR=encode
 LOG_DATE_FORMAT="+%Y-%m-%d_%H:%M:%S"
@@ -21,6 +22,11 @@ POSTS["x264"]="_x264"
 POSTS["x265"]="_x265"
 PROCESS_MARKERS="enc ${!POSTS[@]}"
 
+if [ -z "$2" ]; then
+	SELECTED_CODECS="${!CODECS[@]}"
+else
+	SELECTED_CODECS=$2
+fi
 
 rm -f $LOGFILE
 rm -f $TEMPFILE
@@ -32,106 +38,135 @@ rm -f $TASK_LIST
 # $3 - absolute output file
 # -----
 function encode {
-if [ -f "$3" ]; then
-	echo "$3" has been already transcoded >> $LOGFILE
-	return
-fi
+	if [ -f "$3" ]; then
+		echo "$3" has been already transcoded >> $LOGFILE
+		return
+	fi
 
-ENCODE_OPTIONS=${CODECS[$1]}
+	if [ ! ${CODECS[$1]+_} ]; then
+		echo Unknown codec: \"$1\" not in ${!CODECS[@]} | tee -a $LOGFILE
+		exit -1
+	fi
 
-if [ -z "$ENCODE_OPTIONS" ]; then
-	echo Unknown codec $1 | tee -a $LOGFILE
-	exit -1
-fi
+	ENCODE_OPTIONS=${CODECS[$1]}
 
-echo  `date $LOG_DATE_FORMAT` Transcoding $2 - $1 >> $LOGFILE
-COMMAND="$FFMPEG -y -i \"$2\" $ENCODE_OPTIONS $TEMPFILE"
-echo $COMMAND
-sh -c $COMMAND
+	echo  `date $LOG_DATE_FORMAT` Transcoding $2 - $1 >> $LOGFILE
+	COMMAND="$FFMPEG $EXTRAOPTS -i \"$2\" $ENCODE_OPTIONS $TEMPFILE"
+	echo $COMMAND
+	sh -c $COMMAND
 
-local ret=$?
-if [ $ret -ne 0 ];then
-	echo failed | tee -a $LOGFILE
-	return $ret
-fi
+	local ret=$?
+	if [ $ret -ne 0 ]; then
+		echo failed | tee -a $LOGFILE
+		return $ret
+	fi
 }
 
 function move_temp {
-if [ ! -d `dirname "$1"` ]; then
-	mkdir `dirname "$1"`
-fi
-mv "$TEMPFILE" "$1"
+	if [ ! -d `dirname "$1"` ]; then
+		mkdir `dirname "$1"`
+	fi
+	mv "$TEMPFILE" "$1"
 }
 
 function encode_test {
-case "$1" in
-	$X264_POST)
-		ENCODE_OPTIONS=$X264_ENCODE_OPTIONS
-	;;
-	$X265_POST)
-		ENCODE_OPTIONS=$X265_ENCODE_OPTIONS
-	;;
-*)
-	  echo "Unknown codec: "$1
-	  echo "Unknown codec: "$1 >> $LOGFILE
-	exit -1
-esac
-echo ffmpeg -y -i "$2" $ENCODE_OPTIONS $TEMPFILE
+	if [ ${CODECS[$1]+_} ]; then
+		echo Unknown codec "$1" | tee -a $LOGFILE
+		exit -1
+	fi
+
+	ENCODE_OPTIONS=${CODECS[$1]}
+
+	echo  `date $LOG_DATE_FORMAT` Transcoding $2 - $1 >> $LOGFILE
+	COMMAND="$FFMPEG -y -i \"$2\" $ENCODE_OPTIONS $TEMPFILE"
+	echo $COMMAND
 }
 
-# collect all videos within the input directory
-OIFS=$IFS
-NIFS=$'\n'
-IFS=$NIFS
-#find $SEARCHDIR  -iregex $EXTENSIONS -print0 | while IFS= read -r -d '' video
-#find $SEARCHDIR  -iregex $EXTENSIONS -print0 | while read -d '' -r video
-x=0
-for video in `find $SEARCHDIR -iregex $EXTENSIONS -print0 | xargs -0 -I f echo f`
-do
-	encoding_needed=true
-	IFS=$OIFS
-	for marker in $PROCESS_MARKERS 
+function collect_videos {
+	# collect all videos within the input directory
+	OIFS=$IFS
+	NIFS=$'\n'
+	IFS=$NIFS
+	#find $SEARCHDIR  -iregex $EXTENSIONS -print0 | while IFS= read -r -d '' video
+	#find $SEARCHDIR  -iregex $EXTENSIONS -print0 | while read -d '' -r video
+	x=0
+	for video in `find $SEARCHDIR -iregex $EXTENSIONS -print0 | xargs -0 -I f echo f`
 	do
-		if [[ $video == *$marker* ]]; then
-			echo Skipping: $video  >> $LOGFILE
-			encoding_needed=false
+		encoding_needed=true
+		IFS=$OIFS
+		for marker in $PROCESS_MARKERS
+		do
+			if [[ $video == *$marker* ]]; then
+				echo Skipping: $video  >> $LOGFILE
+				encoding_needed=false
+			fi
+		done
+		if [[ $encoding_needed == "true" ]]; then
+			FILES[$x]=$video
+			((x+=1))
 		fi
 	done
-	if [[ $encoding_needed == "true" ]]; then
-		FILES[$x]=$video
-		((x+=1))
-	fi
-done
+}
 
 #( set -o posix ; set ) | grep FILES
 
 # print/write out the found videos
-IFS=$NIOFS
-for t in "${FILES[@]}"
-do
-	echo $t | tee -a $TASK_LIST
-done
-read -n1 -r -p "Press a key to continue..." key
-
-# prepare the output filenames and start the encoding 
-for CODEC in "${!CODECS[@]}" 
-do
-	POST=${POSTS[$CODEC]}
-	for v in "${FILES[@]}"
+function print_tasklist {
+	for t in "${FILES[@]}"
 	do
-		videofile="$v"
-		fname=`basename $videofile .mp4`
-		targetdir=`dirname $videofile`/$SUBDIR
-#		targetdir=/mnt/data/tmp
-		targetfile=$targetdir/$fname$POST".mp4"
- 		encode $CODEC $videofile $targetfile
-		ret=$?
-		if [ ! $ret -ne 0 ]; then
-			move_temp $targetfile
-			touch -r $videofile "$targetfile"
-			echo "done" >> "$LOGFILE"
-		else
-			echo "failed to move" >> "$LOGFILE"
-fi
+		echo $t | tee -a $TASK_LIST
 	done
-done
+}
+
+
+function process_folder {
+	# prepare the output filenames and start the encoding
+	for c in "${SELECTED_CODECS}"
+	do
+		for v in "${FILES[@]}"
+		do
+			process_video "$v" "$c"
+		done
+	done
+}
+
+# ----
+# $1 - video file
+# $2 - codec option
+# ----
+function process_video {
+	IFS=
+	videofile="$1"
+	CODEC=$2
+	POST=${POSTS[$CODEC]}
+
+	fname=`basename "$videofile" .mp4`
+	targetdir=`dirname "$videofile"`/$SUBDIR
+	targetfile=$targetdir/"$fname"$POST".mp4"
+	encode $CODEC "$videofile" "$targetfile"
+	ret=$?
+	if [ ! $ret -ne 0 ]; then
+		move_temp $targetfile
+		touch -r $videofile "$targetfile"
+		echo "done" >> "$LOGFILE"
+	else
+		echo "failed to transcode" >> "$LOGFILE"
+	fi
+}
+
+echo "Selected codecs: "$SELECTED_CODECS
+
+if [[ -f "$1" ]]; then
+	echo "File processing: \"$1\"" | tee -a $LOGFILE
+	for c in "${SELECTED_CODECS}"
+	do
+		process_video "$1" "$c"
+	done
+fi
+if [[ -d "$1" ]]; then
+	echo "Folder processing: $1" |tee -a $LOGFILE
+	collect_videos
+	print_tasklist
+	read -n1 -r -p "Press a key to continue..." key
+	process_folder
+fi
